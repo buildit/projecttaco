6529d7f56becc302d47eaf78c002e674827d781e
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-01-16 13:38:03 -0500

    cleaned up some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
       { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/, query: {
           presets: ['es2015', 'react'],
         }, },
