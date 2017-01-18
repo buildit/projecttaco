d90669c8e81e31fc258ddf780ae317acebe1dacd
Zachary Smith <william.smith1@wipro.com>
2017-01-18 17:01:10 -0500

    Add footer menu

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,11 +7,16 @@ class Landing extends Component {
       <div className="view landing">
         <h1>Your recent order</h1>
         <div className="landing-image"></div>
-        <div className="cta">
+        <div className="button">
           <Link to="/cart">Re-order</Link>
         </div>
-        <div className="cta">
+        <div className="button">
           <Link to="/location">New Order</Link>
+        </div>
+        <div className="nav">
+          <span className="nav-item">home</span>
+          <span className="nav-item">history</span>
+          <span className="nav-item">location</span>
         </div>
       </div>
 
