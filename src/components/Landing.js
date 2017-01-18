25e04babddddead8f29d669e062805d2315d147f
Zachary Smith <william.smith1@wipro.com>
2017-01-18 16:35:30 -0500

    Build out basic landing view

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,9 +4,15 @@ import { Link } from 'react-router';
 class Landing extends Component {
   render() {
     return(
-      <div>
-        <h1>Landing</h1>
-        <Link to="/location">New Order</Link>
+      <div className="view landing">
+        <h1>Your recent order</h1>
+        <div className="landing-image"></div>
+        <div className="cta">
+          <Link to="/cart">Re-order</Link>
+        </div>
+        <div className="cta">
+          <Link to="/location">New Order</Link>
+        </div>
       </div>
 
     )
