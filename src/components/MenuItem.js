cf89e25938ff994619241b343483e30ba57d227a
Zachary Smith <william.smith1@wipro.com>
2017-01-19 17:10:46 -0500

    Improve alignment of menu items

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,7 +4,7 @@ import QuantityPicker from './QuantityPicker';
 
 const MenuItem = ({ name, price, quantity = 0 }) => (
     <div className="menu-item">
-      <PlaceholderImage width={180} height={180} />
+      <PlaceholderImage width={160} height={160} />
       <div className="info">
           <span className="name">{name}</span>
           <span className="price">{price}</span>
