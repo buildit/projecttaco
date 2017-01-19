4f3a1a1c2629a3296a52c7822617cfe8f5306738
Zachary Smith <william.smith1@wipro.com>
2017-01-19 09:58:03 -0500

    Add TacoCrumbs component, which is the nav-like thing at the top of the screen

---
+++ src/components/TacoCrumbs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TacoCrumbs extends Component {
+  render() {
+    return(
+      <div className="taco-crumbs">
+        <span className="crumb">pickup order</span>
+      </div>
+    )
+  }
+};
+
+export default TacoCrumbs;
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Nav from '../components/nav';
+import Nav from '../components/Nav';
+import TacoCrumbs from '../components/TacoCrumbs';
 
 class Landing extends Component {
   render() {
     return(
       <div className="view landing">
+        <TacoCrumbs />
         <h1>Your recent order</h1>
         <div className="landing-image"></div>
         <div className="button">
