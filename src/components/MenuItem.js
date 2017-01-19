649d76cb886dcd66c3ed6d45e781c3b03033f461
Zachary Smith <william.smith1@wipro.com>
2017-01-19 15:10:14 -0500

    Build out the menu to match the current design
    Add a few menu related components, and style them
    Style view title
    Remove purple. Yuk.
    Style MenuCategoryItems. Allow for reversing of type when needed.
    Add ViewTitle component

---
+++ src/components/MenuItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PlaceholderImage from './PlaceholderImage';
+import QuantityPicker from './QuantityPicker';
+
+const MenuItem = ({ name, price, quantity = 0 }) => (
+    <div className="menu-item">
+      <PlaceholderImage width={180} height={180} />
+      <div className="info">
+          <span className="name">{name}</span>
+          <span className="price">{price}</span>
+      </div>
+      <QuantityPicker quantity={6} />
+    </div>
+);
+
+export default MenuItem;
