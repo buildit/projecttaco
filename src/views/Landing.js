8bfee64f95801e4fedbfc083b8d454995f73a2d6
Zachary Smith <william.smith1@wipro.com>
2017-01-20 17:31:30 -0500

    Change Landing view text

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -9,8 +9,8 @@ export default class Landing extends Component {
   render() {
     return(
       <div className="view landing">
-        <h1>Your recent order</h1>
-        <PlaceholderImage width={500} height={300} label="Photo representing your last order"/>
+        <h1>Welcome back!</h1>
+        <PlaceholderImage width={500} height={300} label="Huge taco comin' atcha"/>
         <div className="button">
           <Link to="/cart">Re-order</Link>
         </div>
