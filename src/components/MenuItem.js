33ed9f98130a4fd44478824519238621641c9926
Zachary Smith <william.smith1@wipro.com>
2017-01-24 17:47:06 -0500

    Use Taco Styles from package for MenuItem

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,7 +4,7 @@ import QuantityPicker from './QuantityPicker';
 
 const MenuItem = ({ name, price, quantity = 0 }) => (
     <div className="menu-item">
-      <PlaceholderImage width={160} height={160} />
+      <PlaceholderImage width={172} height={172} />
       <div className="info">
           <span className="name">{name}</span>
           <span className="price">{price}</span>
