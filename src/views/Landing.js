1ab6cf47422d4d27930cc822acd9708ceb4e624a
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-01-26 15:52:03 -0500

    building out landing page..wip still!! but getting there

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -8,17 +8,29 @@ import PlaceholderImage from '../components/PlaceholderImage';
 export default class Landing extends Component {
   render() {
     return(
-      <div className="view landing">
-        <h1>Welcome back!</h1>
-          <img src="../assets/images/logo.png" srcset="./assets/images/logo@2x.png 2x, ./assets/images/logo@3x.png 3x" class="Logo"></img>
-        <PlaceholderImage width={500} height={300} label="Huge taco comin' atcha"/>
+      <div className="main-container">
+      <div className="landing-container">
+        <div className="logo">
+          <img src="../assets/images/logo.png"
+            srcset="./assets/images/logo@2x.png 2x,
+            ./assets/images/logo@3x.png 3x"
+            />
+        </div>
+
+          <img src="../assets/images/welcome-back.png"
+              srcset="./assets/images/welcome-back@2x.png 2x,
+              ./assets/images/welcome-back@3x.png 3x"
+              className="welcome"/>
         <div className="button">
           <Link to="/cart">Re-order</Link>
         </div>
         <div className="button">
           <Link to="/location">New Order</Link>
         </div>
+        <div>
         <Nav />
+        </div>
+      </div>
       </div>
     )
   }
