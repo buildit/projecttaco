5c38dd022a3861f038abec3d05b9daf5925c3246
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-01-26 13:32:17 -0500

    adding back in some styles and images

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -10,6 +10,7 @@ export default class Landing extends Component {
     return(
       <div className="view landing">
         <h1>Welcome back!</h1>
+          <img src="../assets/images/logo.png" srcset="./assets/images/logo@2x.png 2x, ./assets/images/logo@3x.png 3x" class="Logo"></img>
         <PlaceholderImage width={500} height={300} label="Huge taco comin' atcha"/>
         <div className="button">
           <Link to="/cart">Re-order</Link>
