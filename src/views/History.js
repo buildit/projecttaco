53621e9f986d08e32257258e44755fcc98c0a67f
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-02-04 15:35:27 -0500

    adding confirmation view

--- src/views/History.js
+++ src/views/History.js
@@ -1,15 +1,126 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Nav from '../components/Nav';
-import ViewTitle from '../components/ViewTitle';
+import MenuCategoryListHistory from '../components/MenuCategoryListHistory';
 import { browserHistory } from 'react-router';
 
 class History extends Component {
   render() {
     return(
       <div className="view history">
-        <ViewTitle title="History" />
-        <div>hey your recent orders</div>
+        <div className="header">
+          <div className="back-button" onClick={browserHistory.goBack}>
+            <img src="../assets/images/back-button.png" />
+          </div>
+          <div className="header-text">
+            HISTORY
+          </div>
+          <div className="cart-button">
+          <Link to="/cart">  <img src="../assets/images/cart-icon.png" /></Link>
+          </div>
+        </div>
+        <MenuCategoryListHistory />
+        <div className="order-list-holder">
+        <div className="order-block">
+        <div className="order-info">
+          <span className="order-number">ORDER: 103116</span>
+          <span><img src="../assets/images/bookmark.png" className="bookmark"/></span>
+          <div className="order-address">
+            18 E 14th Street,<br/> New York, NY 10003
+          </div>
+          <div className="reorder">
+            <div className="button-reorder">
+              <Link to="/cart">re-order</Link>
+            </div>
+          </div>
+        </div>
+        <div className="order-image">
+          <img src="../assets/images/order-image.png" className="order-image"/>
+        </div>
+        <div className="order-expand">
+          <img src="../assets/images/expand-up.png"/>
+        </div>
+        </div>
+        <div className="order-history-list">
+          <div className="order-history-item">
+            <span className="history-count">2</span>
+            <span className="history-desc">Cheesy Gordita Crunch</span>
+            <span className="history-price">$5.00</span>
+          </div>
+          <div className="order-history-item">
+            <span className="history-count">2</span>
+            <span className="history-desc">Cheesy Gordita Crunch</span>
+            <span className="history-price">$5.00</span>
+          </div>
+          <div className="order-history-item">
+            <span className="history-count">2</span>
+            <span className="history-desc">Cheesy Gordita Crunch</span>
+            <span className="history-price">$5.00</span>
+          </div>
+          <div className="order-history-item">
+            <span className="history-count">2</span>
+            <span className="history-desc">Cheesy Gordita Crunch</span>
+            <span className="history-price">$5.00</span>
+          </div>
+          <div className="order-history-item">
+            <span className="history-count">2</span>
+            <span className="history-desc">Cheesy Gordita Crunch</span>
+            <span className="history-price">$5.00</span>
+          </div>
+        </div>
+        </div>
+        <div className="total">
+          <span className="subtotal">
+            Subtotal
+          </span>
+          <span className="amount">
+            $11.98
+          </span>
+        </div>
+        <div className="order-list-holder">
+        <div className="order-block">
+        <div className="order-info">
+          <span className="order-number">ORDER: 103116</span>
+          <span><img src="../assets/images/bookmark.png" className="bookmark"/></span>
+          <div className="order-address">
+            18 E 14th Street,<br/> New York, NY 10003
+          </div>
+          <div className="reorder">
+            <div className="button-reorder">
+              <Link to="/cart">re-order</Link>
+            </div>
+          </div>
+        </div>
+        <div className="order-image">
+          <img src="../assets/images/order-image.png" className="order-image"/>
+        </div>
+        <div className="order-expand">
+          <img src="../assets/images/expand.png"/>
+        </div>
+        </div>
+        </div>
+        <div className="order-list-holder">
+        <div className="order-block">
+        <div className="order-info">
+          <span className="order-number">ORDER: 103116</span>
+          <span><img src="../assets/images/bookmark.png" className="bookmark"/></span>
+          <div className="order-address">
+            18 E 14th Street,<br/> New York, NY 10003
+          </div>
+          <div className="reorder">
+            <div className="button-reorder">
+              <Link to="/cart">re-order</Link>
+            </div>
+          </div>
+        </div>
+        <div className="order-image">
+          <img src="../assets/images/order-image.png" className="order-image"/>
+        </div>
+        <div className="order-expand">
+          <img src="../assets/images/expand.png"/>
+        </div>
+        </div>
+        </div>
         <Nav />
       </div>
     )
--- src/views/Receipt.js
+++ src/views/Receipt.js
@@ -8,8 +8,49 @@ class Receipt extends Component {
   render() {
     return(
       <div className="view receipt">
-        <ViewTitle title="Receipt" />
-        <div>hey your receipt</div>
+        <div className="header">
+          <div className="header-text">
+            order confirmation
+          </div>
+          <div className="cart-button">
+          <Link to="/">  <img src="../assets/images/close-button.png" /></Link>
+          </div>
+        </div>
+        <div className="hero">
+          <div className="yass">
+            <img src="../assets/images/yass-time-to-feast.png" />
+          </div>
+        </div>
+        <div className="order-large">
+          Order: 012546
+        </div>
+        <div className="pick-up">
+          <div className="pick-up-location">
+            Pick up location
+          </div>
+          <div className="pick-up-time">
+            pick up time
+          </div>
+        </div>
+        <div className="pick-up-info">
+          <div className="pick-up-address">
+            18 E. 14th Street New York, NY 10003
+          </div>
+          <div className="pick-up-ready-time">
+            Ready after 02:35pm
+          </div>
+        </div>
+        <div className="pick-up-link">
+          <div className="directions">
+            get directions
+          </div>
+          <div className="pick-up-later">
+            schedule for later
+          </div>
+        </div>
+        <div className="button-add-order">
+          <Link to="/menu">add an order</Link>
+        </div>
         <Nav />
       </div>
     )
