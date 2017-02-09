892d482ea7d410e01d94a80ca84206b96e241406
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-02-09 10:20:49 -0500

    updating comp

--- src/components/TacoCrumbs.js
+++ src/components/TacoCrumbs.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { browserHistory } from 'react-router';
 
 const TacoCrumbs = ({ viewName }) => (
-  <div className="taco-crumbs">
-    <span className="back-icon">back</span>
-    <span className="action">{ viewName }</span>
-    <span className="cart-icon">cart</span>
+  <div className="header">
+    <div className="back-button" className="back-button" onClick={browserHistory.goBack}>
+      <img src="../assets/images/back-button.png" />
+    </div>
+    <div className="header-text">
+      { viewName }
+    </div>
+    <div className="cart-button">
+      <Link to="/cart"><img src="../assets/images/cart-icon.png" /></Link>
+      <span>13</span>
+    </div>
   </div>
 )
 
